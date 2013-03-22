4bb66aa26a99825c73b2ed7c2a0fed04dca89072
Jeffrey Hu <zhiwehu@gmail.com>
2013-03-22 16:22:12 +0800

    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'

--- django_aws_s3_static_web/django_aws_s3_static_web/settings/production.py
+++ django_aws_s3_static_web/django_aws_s3_static_web/settings/production.py
@@ -76,10 +76,11 @@ AWS_SECRET_ACCESS_KEY = get_env_setting('AWS_SECRET_ACCESS_KEY')
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-DEBUG=True
+DEBUG=False
 
 ALLOWED_HOSTS=['s3staticweb.herokuapp.com',]
 
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME='s3staticweb'
 AWS_S3_SECURE_URLS=False
